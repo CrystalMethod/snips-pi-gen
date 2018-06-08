2b2d52393482bec472e4a2b28b4396a22e82e854
Lars Martin <lars.martin@smb-tec.com>
2018-06-08 16:33:27 +0200

    add configuration to start access point; add initial implementation of configuration app

--- assets/03-snips/00-run.sh
+++ assets/03-snips/00-run.sh
@@ -6,3 +6,15 @@ on_chroot << EOF
 pip install virtualenv
 apt-get update
 EOF
+
+install -m 644 files/hotspot.txt "${ROOTFS_DIR}/boot/"
+install -m 644 files/interfaces "${ROOTFS_DIR}/etc/network/"
+install -m 644 files/hostapd "${ROOTFS_DIR}/etc/default/"
+install -m 644 files/hostapd.conf "${ROOTFS_DIR}/etc/hostapd/"
+install -m 644 files/snips-access-point.conf "${ROOTFS_DIR}/etc/dnsmasq.d/" 
+install -m 644 files/snips-access-point.service "${ROOTFS_DIR}/etc/systemd/system/"
+install -m 755 files/snips-access-point "${ROOTFS_DIR}/usr/bin"
+on_chroot << EOF
+echo "10.99.99.1      snips" | sudo tee --append /etc/hosts
+systemctl enable snips-access-point.service
+EOF
--- assets/03-snips/01-run.sh
+++ assets/03-snips/01-run.sh
@@ -1,5 +1,7 @@
 on_chroot << EOF
 usermod -a -G snips-skills-admin pi
+systemctl stop snips-asr-google
 systemctl disable snips-asr-google
+systemctl stop snips-asr-injection
 systemctl disable snips-asr-injection
 EOF
